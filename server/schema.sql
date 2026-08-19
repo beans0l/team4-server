f5bd66c93795667f05f62a5053959c9a362cc1ec
geonmooklim0912 <geonmooklim0912@gmail.com>
2026-08-19 23:03:37 +0900

    데이터베이스 수정

--- server/schema.sql
+++ server/schema.sql
@@ -6,7 +6,7 @@
 
 CREATE TABLE IF NOT EXISTS users (
     id            INT AUTO_INCREMENT PRIMARY KEY,
-    account       VARCHAR(50)  NOT NULL UNIQUE,
+    username       VARCHAR(50)  NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     email         VARCHAR(255) UNIQUE,
     name          VARCHAR(30),
